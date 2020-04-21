730aa7132dd06622b86dc6e37647b5fab25debc7
Robert Garza <garza.robert28@gmail.com>
2020-04-21 17:20:28 -0500

    added navbar to high scores page

--- script2.js
+++ script2.js
@@ -1,8 +1,8 @@
-// var userLast = document.querySelector("#firstPlace");
+var userLast = document.querySelector("#firstPlace");
 
 
 
 
 
-// var lastUser = JSON.parse(localStorage.getItem("user"));
-// userLast.textcontent = lastUser.userName;
+var lastUser = JSON.parse(localStorage.getItem("user"));
+userLast.textcontent = lastUser.userName;
